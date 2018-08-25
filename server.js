428556046bb283f955772b334619351a3087dfd5
Madan Gopal Singh <madangopal.singh792@gmail.com>
2018-08-25 20:06:47 +0530

    fixes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/rankning-demo-app'));
 
@@ -11,6 +14,3 @@ app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/ranking-demo-app/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
