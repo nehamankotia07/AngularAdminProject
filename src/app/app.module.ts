ca2d65acd626f0d91f2d94c7eaa86ee7f892c1e1
Madan Gopal Singh <madangopal.singh792@gmail.com>
2018-08-25 17:45:33 +0530

    removed in-memory service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http'
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
-import { InMemoryDataService } from './in-memory-data.service'
 import { AppComponent } from './app.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,10 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
