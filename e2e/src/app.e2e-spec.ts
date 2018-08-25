191c69c578eea20d7743e8912502cc94198db322
Madan Gopal Singh <madangopal.singh792@gmail.com>
2018-08-25 18:12:44 +0530

    app name changed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to clientapp25!');
+    expect(page.getParagraphText()).toEqual('Welcome to ranking-demo-app!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'clientapp25'`, async(() => {
+  it(`should have as title 'Ranking Demo App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('clientapp25');
+    expect(app.title).toEqual('Ranking Demo App');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to clientapp25!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Ranking Demo App!');
   }));
 });
