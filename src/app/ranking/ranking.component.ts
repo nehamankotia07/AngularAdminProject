5daef2fba02ef3a99fa089b0c85b75e8e1bdd205
Madan Gopal Singh <madangopal.singh792@gmail.com>
2018-08-26 21:48:53 +0530

    Update functionality added

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -12,8 +12,12 @@ export class RankingComponent implements OnInit {
 
   rankingData: any = [];
   nodeFound: boolean = false;
+
   dataName: string;
   parentId: number;
+
+  dataNameUpdate: string;
+  nodeId: number;
 
   constructor(private rankingService: RankingService) { }
 
@@ -28,7 +32,6 @@ export class RankingComponent implements OnInit {
   public findRankingByParentIdAndAdd(idOfNode: number, parentId: number, dataName: string, rankingD: any) {
     if (!this.nodeFound) {
       for(let rd of rankingD) {
-        //if (rd != null) {
           if(rd.Id == parentId) {
             if (rd.Nodes == null) {
               rd.Nodes = [];
@@ -43,9 +46,24 @@ export class RankingComponent implements OnInit {
               this.findRankingByParentIdAndAdd(idOfNode, parentId, dataName, rd.Nodes);
             }
           }
-        //} else {
-        //  return;
-        //}
+      }
+    } else {
+      return;
+    }
+  }
+
+  public findRankingByParentIdAndUpdate(nodeId: number, dataName: string, rankingD: any) {
+    if (!this.nodeFound) {
+      for(let rd of rankingD) {
+          if(rd.Id == nodeId) {
+            rd.Name = dataName;
+            this.nodeFound = true;
+            return;
+          } else {
+            if (rd.Nodes != null){
+              this.findRankingByParentIdAndUpdate(nodeId, dataName, rd.Nodes);
+            }
+          }
       }
     } else {
       return;
@@ -55,5 +73,11 @@ export class RankingComponent implements OnInit {
   public addDatainRankingData(): void {
     this.nodeFound = false;
     this.findRankingByParentIdAndAdd(this.parentId+100, this.parentId, this.dataName, this.rankingData);
+  }
+
+  public updateDatainRankingData(): void {
+    this.nodeFound = false;
+    this.findRankingByParentIdAndUpdate(this.nodeId, this.dataNameUpdate, this.rankingData);
+
   }
 }
